86136342f0261be416c234f75ea5bac18b9dcd9c
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2023-09-24 17:10:15 +0300

    Add regular stock lookup functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,12 +2,24 @@ require 'httparty'
 
 class Stock < ApplicationRecord
 
+  def self.company_lookup(ticker_symbol)
+    response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage_client[:alphavantage_api_key]}")
+    if response.code == 200
+      response.parsed_response['Name']
+    else
+      nil
+    end 
+  end
+
   def self.new_lookup(ticker_symbol)
-    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=Rails.application.credentials.alphavantage_client[:alphavantage_api_key]")
+    response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage_client[:alphavantage_api_key]}")
     if response.code == 200 && response.parsed_response['Global Quote']
-      response.parsed_response['Global Quote']['05. price']
+      last_price = response.parsed_response['Global Quote']['05. price']
+      name = company_lookup(ticker_symbol)
+      new(ticker: ticker_symbol, name: name, last_price: last_price)
     else
       nil
     end
   end
-end 
+
+end
